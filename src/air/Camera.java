00efcd232d250e8f1ff558f9aeb385eeaad06b86
Aaron Bell <aaron.bell@gmail.com>
2011-10-16 14:17:45 -0400

    camera tracking

---
+++ src/air/Camera.java
@@ -0,0 +1,59 @@
+package air;
+
+import java.awt.Dimension;
+import java.awt.Point;
+
+public class Camera {
+    /** topleft coords of the viewport */
+    public Point position = new Point(0, 0);
+
+    protected Dimension size = new Dimension(0, 0);
+    protected Dimension world = new Dimension(0, 0);
+    protected Point maxPosition = new Point(0, 0);
+
+    public Camera(int worldWidth, int worldHeight, int cameraWidth, int cameraHeight) {
+        world.width = worldWidth;
+        world.height = worldHeight;
+        size.width = cameraWidth;
+        size.height = cameraHeight;
+        maxPosition = new Point(world.width - size.width, world.height - size.height);
+    }
+
+    public void centreCameraOn(int x, int y) {
+        int targetX = (int) (x - (size.width / 2));
+        int targetY = (int) (y - (size.height / 2));
+        setViewport(targetX, targetY);
+    }
+
+    /** TODO: pushMatrix, translate, popMatrix */
+    public Point translateWorldToCamera(int x, int y) {
+        // transform.translate((obj.location.x - viewport.x), (obj.location.y - viewport.y));
+        return new Point(x - position.x, y - position.y);
+    }
+
+    /** move the viewport's top-left corner to this location */
+    protected void setViewport(int x, int y) {
+        if (x > maxPosition.x) {
+            // log("can't set viewport to x " + x + ", capping at " + maxPosition.x);
+            x = maxPosition.x;
+        }
+        else if (x < 0) {
+            // log("can't set viewport to x " + x + ", capping at 0");
+            x = 0;
+        }
+        if (y > maxPosition.y) {
+            // log("can't set viewport to y " + y + ", capping at " + maxPosition.y);
+            y = maxPosition.y;
+        }
+        else if (y < 0) {
+            // log("can't set viewport to y " + y + ", capping at 0");
+            y = 0;
+        }
+        position.x = x;
+        position.y = y;
+    }
+
+    protected void log(String msg) {
+        System.out.println(msg);
+    }
+}
--- src/air/ViewportTest.java
+++ src/air/ViewportTest.java
@@ -1,24 +1,75 @@
 package air;
 
 import java.awt.Dimension;
+import java.awt.Point;
+
+import processing.core.PGraphics2D;
 
 public class ViewportTest extends Sketch {
-    Dimension canvas = new Dimension(1000, 1000);
-    Dimension screen = new Dimension(500, 500);
+    Dimension world = new Dimension(600, 300);
+    Dimension cameraSize = new Dimension(600, 300);
 
     final int GRID_SIZE = 100;
     final int gridColor = color(0, 255, 0);
 
-    Viewport viewport = new Viewport(canvas.width - screen.width, canvas.height - screen.height);
+    Camera camera = new Camera(world.width, world.height, cameraSize.width, cameraSize.height);
+
+    Point dummy = new Point(1, 1);
+    Point velocity = new Point((int) random(2, 10), (int) random(1, 10));
 
     @Override
     public void setup() {
-        size(screen.width, screen.height);
+        size(cameraSize.width, cameraSize.height, PGraphics2D.P2D);
+        background(0);
+        stroke(255);
     }
 
     @Override
     public void draw() {
         showDebugAtIntervals();
 
+        update();
+        camera.centreCameraOn(dummy.x, dummy.y);
+        background(0);
+        drawGrid();
+        drawObjects();
+    }
+
+    protected void update() {
+        dummy.x = dummy.x + velocity.x;
+        if (dummy.x >= world.width) {
+            dummy.x = world.width;
+            velocity.x = -velocity.x;
+        }
+        if (dummy.x < 0) {
+            dummy.x = 0;
+            velocity.x = -velocity.x;
+        }
+
+        dummy.y = dummy.y + velocity.y;
+        if (dummy.y >= world.height) {
+            dummy.y = world.height;
+            velocity.y = -velocity.y;
+        }
+        if (dummy.y < 0) {
+            dummy.y = 0;
+            velocity.y = -velocity.y;
+        }
+
+    }
+
+    protected void drawGrid() {
+        Point dot;
+        for (int gridy = 0; gridy < world.height; gridy += GRID_SIZE) {
+            for (int gridx = 0; gridx < world.width; gridx += GRID_SIZE) {
+                dot = camera.translateWorldToCamera(gridx, gridy);
+                point(dot.x, dot.y);
+            }
+        }
+    }
+
+    protected void drawObjects() {
+        Point dot = camera.translateWorldToCamera(dummy.x, dummy.y);
+        point(dot.x, dot.y);
     }
 }
