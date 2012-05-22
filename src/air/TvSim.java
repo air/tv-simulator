5538412ea8123588e8282828aa468cd858074e4b
Aaron Bell <aaron.bell@gmail.com>
2012-05-21 21:08:18 -0400

    latest from laptop

--- src/air/TvSim.java
+++ src/air/TvSim.java
@@ -5,21 +5,45 @@ import java.awt.Dimension;
 import processing.core.PImage;
 
 /**
- * TODO it's not actually smooth camera movement - focus is smooth but camera isn't TODO zoom!
+ * TODO fix moving camera, decide how to handle initial view
  * 
+ * TODO switch between edit modes Brightness, Zoom, Interference, Pan
+ * 
+ * TODO make a generic scaleWithMouse(xOrY, LOGARITHMIC, min, max)
+ * 
+ * TODO better images, more meme-y. Reddit. Trollface. Mudkips.
+ * 
+ * TODO demo mode - toggle R G B off in a sequence, move around, zoom
+ * 
+ * TODO bloom at extreme zooms
+ * 
+ * TODO sub-pixel accuracy for better zoomed out
+ * 
+ * TODO higher res images
+ * 
+ * TODO surround zoomed-out image with something else; at least grey backdrop
+ * 
+ * TODO video
+ * 
+ * TODO sound, hum and interference noise
  */
 @SuppressWarnings("serial")
 public class TvSim extends Sketch {
     PImage image;
 
-    Dimension world; // set by image size
-    Dimension cameraSize = new Dimension(800, 800);
+    Dimension world; // this is set by the loaded image
+    Dimension cameraSize = new Dimension(1400, 900); // bigger is slower
     Camera camera;
-    OrbitingPoint focus;
+    boolean movingCamera = false;
+    OrbitingPoint movingCameraFocus;
 
+    boolean zoomOn = false;
+    final float MAX_ZOOM = 770f; // TODO bigger than this fails for some reason
+
+    boolean controlBrightnessOn = false;
     float brightness;
-    final int MAX_BRIGHTNESS = 2; // used to scale the brightness by mouse position
-    int BRIGHTNESS_COEFF; // how much to scale the brightness based on sketch width
+    final float MAX_BRIGHTNESS = 1.5f; // we can overload the brightness by this much
+    float BRIGHTNESS_COEFF; // how much to scale the brightness based on sketch width
 
     boolean interference = false;
     // TODO there is a noiseDetail() method which we are probably simulating
@@ -32,10 +56,14 @@ public class TvSim extends Sketch {
     final float MAX_PERLIN_COEFF = 3000f;
     final float MIN_PERLIN_COEFF = 0.001f;
 
-    final int CELL_WIDTH = 15; // try (phos width * 3) + 2
-    final int CELL_HEIGHT = 14; // try phos height + 1
-    final int PHOSPHOR_WIDTH = 4; // phosphor width * 3 must fit in cell width!
-    final int PHOSPHOR_HEIGHT = 12;
+    // final int CELL_WIDTH = 15; // try (phos width * 3) + 2
+    // final int CELL_HEIGHT = 14; // try phos height + 1
+    // final int PHOSPHOR_WIDTH = 4; // phosphor width * 3 must fit in cell width!
+    // final int PHOSPHOR_HEIGHT = 12;
+    final int CELL_WIDTH = 5; // try (phos width * 3) + 2
+    final int CELL_HEIGHT = 4; // try phos height + 1
+    final int PHOSPHOR_WIDTH = 1; // phosphor width * 3 must fit in cell width!
+    final int PHOSPHOR_HEIGHT = 3;
 
     boolean redOn = true;
     boolean greenOn = true;
@@ -47,18 +75,21 @@ public class TvSim extends Sketch {
         image = loadImage("rgbtest150.jpg");
 
         world = new Dimension(image.width * CELL_WIDTH, image.height * CELL_HEIGHT);
-        size(cameraSize.width, cameraSize.height, P2D);
+        // size(cameraSize.width, cameraSize.height, P2D); // no support for Z scale
+        // size(cameraSize.width, cameraSize.height, OPENGL); // twice as slow as P3D
+        size(cameraSize.width, cameraSize.height, P3D);
+
         camera = new Camera(world.width, world.height, cameraSize.width, cameraSize.height);
 
-        int centreX = world.width / 2;
-        int centreY = world.height / 2;
-        focus = new OrbitingPoint(centreX, centreY, centreX, centreY);
+        int maxCamX = world.width - cameraSize.width;
+        int maxCamY = world.height - cameraSize.height;
+        movingCameraFocus = new OrbitingPoint(maxCamX / 2, maxCamY / 2, maxCamX / 2, maxCamY / 2);
 
         // for the width of the sketch to scale to the max brightness, we need to take the number of pixels
         // in the image and expand it out by the cell width
-        BRIGHTNESS_COEFF = (image.width * CELL_WIDTH) / MAX_BRIGHTNESS;
+        BRIGHTNESS_COEFF = cameraSize.width / MAX_BRIGHTNESS;
         // set the initial brightness as if the mouse were at the right edge
-        brightness = width / BRIGHTNESS_COEFF;
+        brightness = cameraSize.width / BRIGHTNESS_COEFF;
 
         noStroke();
         background(0);
@@ -75,13 +106,11 @@ public class TvSim extends Sketch {
         // log("perlin_coeff %.5f", perlin_coeff);
         // log("mouseY " + mouseY);
         // log("height " + height);
-        // log("brightness " + brightness);
     }
 
     /**
-     * We take the position in the perlin landscape - a simple incrementing int - and divide by this to pass into noise()
-     * 
-     * TODO: make a generic scaleWithMouse(xOrY, LOGARITHMIC, min, max)
+     * We take the position in the perlin landscape - a simple incrementing int - and divide by this to pass into
+     * noise()
      */
     protected float getPerlinCoeff() {
         int steps = height;
@@ -95,18 +124,34 @@ public class TvSim extends Sketch {
     }
 
     protected float getBrightnessCoeff() {
-        return (float) mouseX / BRIGHTNESS_COEFF;
+        return mouseX / BRIGHTNESS_COEFF;
+    }
+
+    protected void handleLeftButtonDown() {
+        // log("zoom %.2f", scaleLinearlyWithMouseX(-770, 770));
     }
 
     @Override
     public void draw() {
         showDebugAtIntervals();
+        if (mousePressed) {
+            handleLeftButtonDown();
+        }
 
-        focus.update();
-        camera.centreCameraOn(focus);
-        translate(-camera.position.x, -camera.position.y);
+        if (movingCamera) {
+            movingCameraFocus.update();
+            camera.setViewport(movingCameraFocus.x, movingCameraFocus.y);
+        }
 
-        if (mouseMoved) {
+        if (zoomOn) {
+            float zoom = scaleLinearlyWithMouseX(-MAX_ZOOM, MAX_ZOOM);
+            translate(-camera.position.x, -camera.position.y, zoom);
+        }
+        else {
+            translate(-camera.position.x, -camera.position.y);
+        }
+
+        if (controlBrightnessOn && mouseMoved) {
             brightness = getBrightnessCoeff();
         }
 
@@ -140,7 +185,9 @@ public class TvSim extends Sketch {
                     else if (new_cell_perturb == cell_perturb) {
                         timesSameNoiseValueReturned++;
                     }
-                    else cell_perturb = new_cell_perturb;
+                    else {
+                        cell_perturb = new_cell_perturb;
+                    }
 
                     // adjust this cell's brightness
                     cell_brightness = brightness * cell_perturb;
@@ -171,27 +218,57 @@ public class TvSim extends Sketch {
         }
     }
 
+    @Override
     public void keyReleased() {
-        if (key == 'r')
+        if (key == 'r') {
             redOn = !redOn;
-        else if (key == 'g')
+        }
+        else if (key == 'g') {
             greenOn = !greenOn;
-        else if (key == 'b')
+        }
+        else if (key == 'b') {
             blueOn = !blueOn;
-        else if (key == 'i')
+        }
+        else if (key == 'i') {
             interference = !interference;
-        else if (key == '1')
+        }
+        else if (key == 'c') {
+            movingCamera = !movingCamera;
+        }
+        else if (key == 'z') {
+            zoomOn = !zoomOn;
+        }
+        else if (key == '1') {
             changeChannel("rgbtest150.jpg");
-        else if (key == '2')
+        }
+        else if (key == '2') {
             changeChannel("rgbgamut150.jpg");
-        else if (key == '3')
+        }
+        else if (key == '3') {
             changeChannel("mondrian150.jpg");
-        else if (key == '4')
+        }
+        else if (key == '4') {
             changeChannel("field150.jpg");
-        background(0);
-        drawRgb(CELL_WIDTH, CELL_HEIGHT);
+        }
     }
 
+    @Override
+    public void keyPressed() {
+        if (keyCode == UP) {
+            camera.position.y -= 10;
+        }
+        else if (keyCode == DOWN) {
+            camera.position.y += 10;
+        }
+        else if (keyCode == LEFT) {
+            camera.position.x -= 10;
+        }
+        else if (keyCode == RIGHT) {
+            camera.position.x += 10;
+        }
+    }
+
+    @Override
     public void mouseMoved() {
         mouseMoved = true;
     }
